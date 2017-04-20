ddaa2a26afb31c38d75ede74ced372c17b5b5670
wangshengwen <wswen1220@163.com>
2017-04-20 18:57:56 +0800

    reactapp

--- index.android.js
+++ index.android.js
@@ -10,14 +10,43 @@ import {
   StyleSheet,
   Text,
   View
-} = React;
+} from 'react-native';
 
-var demo = React.createClass({
-   render: function(){
-       return (
-       <View styz
-       )
-   }
-})
+export default class reactapp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          欢迎来到React的世界!!!
+        </Text>
+        <Text style={styles.instructions}>
+          修改 index.android.js 启动这个程序
+        </Text>
+        <Text style={styles.instructions}>
+          双击R重新编译这个程序
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
 AppRegistry.registerComponent('reactapp', () => reactapp);
